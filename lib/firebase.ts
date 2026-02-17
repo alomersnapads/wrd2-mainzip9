57d6b1561c00776e936bb9ad5fcd1eca48934377
alomersnapads <alomersnapads@gmail.com>
2026-02-17 11:47:48 +0300

    Update firebase.ts

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -3,7 +3,6 @@ import { getDatabase } from 'firebase/database';
 import { doc, getFirestore, setDoc} from 'firebase/firestore';
 
 const firebaseConfig = {
-onst firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
   databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
